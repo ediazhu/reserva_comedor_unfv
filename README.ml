ff2855339d2bae29f740308ce0bfd3fa24e72f72
Eduardo Diaz Huayhuas <ediaz100pre@hotmail.com>
2024-08-03 01:06:39 -0500

    Update README.ml
    Loll

--- README.ml
+++ README.ml
@@ -1,1 +1,9 @@
 
+##Aplicacion para reservar el comedor sin necesidad de loguearte 
+
+Objetivos de la aplicación.
+
+1. Saltarse el tedioso login al sistema
+2. Puedes reservar el almuerzo sin sufrir el problema de conectividad por cuello de botella.
+3. Puedes reservar a un compañero/a con el mismo programa.
+4. Los cupos son limitados y a nadie le gusta hacer cola para la carroña.
