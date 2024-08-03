81ade79b292309de0a3041fd5d4b02a32f06a30d
Eduardo Diaz <ediazh@unsa.edu.pe>
2024-08-03 01:40:36 -0500

    sube archivo

--- reserva_py.py
+++ reserva_py.py
@@ -3,6 +3,7 @@ import cherrypy
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        return "Hello World!"
+        return "Bienvenido a la aplicación para reservar comedor."
+        return "La hora actual es: #HORA"
 
 cherrypy.quickstart(HelloWorld())
